b235f0345ed85c02839518cb2d75625df3d6ae07
Nickk888 <hlllzbb9@mail.ru>
2016-12-30 20:21:41 +0400

    новое окно

--- src/Start.java
+++ src/Start.java
@@ -1,9 +1,18 @@
+import javax.swing.*;
+import java.awt.FlowLayout;
+import javax.swing.JButton;
+
 /**
  * Created by R2-D2 on 28.12.2016.
  */
 public class Start {
 
-        public static fargo(){
-
+        public Start (){
+                JFrame frame = new JFrame("Нажми!" );
+                frame.setSize(200,100);
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setLayout(new FlowLayout());
+                frame.add(new JButton("Не нажимай!"));
+                frame.setVisible(true);
         }
 }
